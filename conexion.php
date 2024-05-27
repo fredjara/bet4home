06215eadb3f0329809f311a23038bb3131b65262
fredjara <166759256+fredjara@users.noreply.github.com>
2024-05-26 22:45:09 -0400

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,14 +1,6 @@
 <?php
 
-/* 
- * $conn = mysqli_init();
-mysqli_ssl_set($conn,NULL,NULL, "home/site/wwwroot/bd/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
-mysqli_real_connect($conn, 'lanzamientobet4home052024.mysql.database.azure.com', 'lanzamientobet4home', 'Bet4#1256', 'lanzamiento', 3306, MYSQLI_CLIENT_SSL);
-if (mysqli_connect_errno()) {
-die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
- * 
- */
+
 
 $conn = mysqli_init();
 mysqli_ssl_set($conn,NULL,NULL, "home/site/wwwroot/bd/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
@@ -16,20 +8,6 @@ mysqli_real_connect($conn, 'lanzamientobet4home052024.mysql.database.azure.com',
 if (mysqli_connect_errno()) {
 die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
-
-/*
-$conexion = new mysqli("localhost", "root", "", "bet4homeform");*$conexion->set_charset('utf8');
-
-    try{
-        $conexion;
-    } catch (Exception $ex) {
-
-    }
-        if ($conexion){
-            echo 'conectado';    
-        }else{
-            echo 'no conectado';
-        }
 
 
 
